bd0654010479920b6fc5f5624a17fa02c72a46e6
Hyanar <danscottbrown@gmail.com>
2021-04-28 00:06:57 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,33 +1,33 @@
 local shinestr = -1
 
 DoMoonshine = function(time)
-  print("moonshine start")
+  --print("moonshine start")
   shinestr = math.random(1,100)
   print(shinestr)
 
   if shinestr >= 0 and shinestr < 24 then
-    print("lvl 1")
+    --print("lvl 1")
     anim = "move_m@drunk@slightlydrunk"
     shake = 0.5
     screenfx = "DRUG_gas_huffin"
     gettingPissed(anim, shake, screenfx)
 
   elseif shinestr >= 25 and shinestr < 50 then
-    print("lvl 2")
+    --print("lvl 2")
     anim = "move_m@drunk@slightlydrunk"
     shake = 1.0
     screenfx = "v_dark"
     gettingPissed(anim, shake, screenfx)
 
   elseif shinestr >= 51 and shinestr < 74 then
-    print("lvl 3")
+    --print("lvl 3")
     anim = "move_m@drunk@moderatedrunk"
     shake = 1.49
     screenfx = "BikerFilter"
     gettingPissed(anim, shake, screenfx)
 
   elseif shinestr >= 75 then
-    print("lvl 4")
+    --print("lvl 4")
     anim = "move_m@drunk@verydrunk"
     shake = 1.49
     screenfx = "BeastIntro01"
@@ -41,8 +41,8 @@ DoMoonshine = function(time)
   ResetPedMovementClipset(GetPlayerPed(-1), 0)
   RemoveAnimSet()
   shinestr = -1
-  print(shinestr)
-  print("moonshine end")
+  --print(shinestr)
+  --print("moonshine end")
 end
 
 gettingPissed = function(anim, shake, screenfx)
